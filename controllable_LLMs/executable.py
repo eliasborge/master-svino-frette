a9d6b770056ce77d3e920cdf2f696e6205f5f56a
Elias Borge Svinø <94860474+eliasborge@users.noreply.github.com>
2025-02-27 11:30:36 +0100

    Uncomment lines

--- controllable_LLMs/executable.py
+++ controllable_LLMs/executable.py
@@ -10,64 +10,64 @@ import pandas as pd
 
 model = "hf.co/bartowski/Llama-3.2-3B-Instruct-GGUF:Q6_K_L"
 
-# data = pd.read_csv("data/grouped_data_from_stormfront/grouped_stormfront_data_2014.csv")
+data = pd.read_csv("data/grouped_data_from_stormfront/grouped_stormfront_data_2014.csv")
 
 
-# ##### USED FOR VERYFIYING THE SYSTEM #####
-# example_agent = ExampleAgent(model)
+##### USED FOR VERYFIYING THE SYSTEM #####
+example_agent = ExampleAgent(model)
 
-# output = example_agent.__call__()
-# print(output)
+output = example_agent.__call__()
+print(output)
 
-# # Replace with actual messages from preprocessing script
+# Replace with actual messages from preprocessing script
 
-# data_random_3 = data.sample(n=3)
-# first_item_content_list_str = eval(data_random_3.iloc[0]['content_list'])
-# print(first_item_content_list_str)
-
-
+data_random_3 = data.sample(n=3)
+first_item_content_list_str = eval(data_random_3.iloc[0]['content_list'])
+print(first_item_content_list_str)
 
 
-# for index,row in data_random_3.iterrows():
+
+
+for index,row in data_random_3.iterrows():
     
-#     content_list = eval(row['content_list'])
-#     content = "".join(content_list)
-#     topic = row['stormfront_topic']
+    content_list = eval(row['content_list'])
+    content = "".join(content_list)
+    topic = row['stormfront_topic']
 
-#     print(topic)
-#     print("Content length: ", row['content_length'])
+    print(topic)
+    print("Content length: ", row['content_length'])
     
 
-#     #Emotion analysis
-#     #emotion_agent = EmotionAgent(model)
-#     #emotions = emotion_agent.__call__(message)
-#     #print(emotions)
+    #Emotion analysis
+    #emotion_agent = EmotionAgent(model)
+    #emotions = emotion_agent.__call__(message)
+    #print(emotions)
 
-#     #Target group analysis
-#     # target_group_agent = TargetGroupAgent(model)
-#     # target_group = target_group_agent.__call__(message)
-#     # print(target_group)
+    #Target group analysis
+    # target_group_agent = TargetGroupAgent(model)
+    # target_group = target_group_agent.__call__(message)
+    # print(target_group)
 
-#     #Otherness analysis
-#     # otherness_agent = OthernessAgent(model)
-#     # otherness = otherness_agent.__call__(message)
-#     # print(otherness)
+    #Otherness analysis
+    # otherness_agent = OthernessAgent(model)
+    # otherness = otherness_agent.__call__(message)
+    # print(otherness)
    
-#     # if(otherness['othernessBoolean'] == "True"):
+    # if(otherness['othernessBoolean'] == "True"):
     
 
-#     otherness_agent = OthernessAgent(model)
-#     otherness = otherness_agent.__call__(content)
-#     print(otherness)
+    otherness_agent = OthernessAgent(model)
+    otherness = otherness_agent.__call__(content)
+    print(otherness)
 
-#     #Framing agent
-#     framing_agent = FramingAgent(model)
-#     framing = framing_agent.__call__(content)
-#     print(framing)
+    #Framing agent
+    framing_agent = FramingAgent(model)
+    framing = framing_agent.__call__(content)
+    print(framing)
 
-#     if( otherness['othernessBoolean'] == "True" or "False"):
-#         intent_agent = IntentAgent(model)
-#         intent = intent_agent.__call__(content, otherness['targetGroup'], framing)
-#         print(intent)
+    if( otherness['othernessBoolean'] == "True" or "False"):
+        intent_agent = IntentAgent(model)
+        intent = intent_agent.__call__(content, otherness['targetGroup'], framing)
+        print(intent)
 
 
