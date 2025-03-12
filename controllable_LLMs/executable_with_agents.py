4b2a44b074abc5e21ccf3f76faaf5ae742baac83
Elias Borge Svinø <94860474+eliasborge@users.noreply.github.com>
2025-03-12 15:37:03 +0100

    Change model

--- controllable_LLMs/executable_with_agents.py
+++ controllable_LLMs/executable_with_agents.py
@@ -11,7 +11,7 @@ from json import loads
 
 import pandas as pd
 
-model = "mistral-small"
+model = "wizardlm2:7b"
 
 df = pd.read_csv("data/testdata/processed_VideoCommentsThreatCorpus.csv")
 grouped_df = pd.read_csv("data/testdata/grouped_processed_VideoCommentsThreatCorpus.csv")
@@ -78,5 +78,5 @@ for index,row in grouped_messages.iterrows():
         
 ### COLLECTION OF DATA ###
 
-    collected_data.to_csv("data/collected_with_agents.csv",index=False)
+    collected_data.to_csv("data/collected_with_agents_wizardllm.csv",index=False)
 
