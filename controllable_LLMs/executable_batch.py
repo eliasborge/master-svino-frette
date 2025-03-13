26eae2d0de936d556f19cf7517b8aea74f3b731d
Thomas Addisu Frette <thomas.frette@gmail.com>
2025-03-13 10:00:35 +0100

    testing

--- controllable_LLMs/executable_batch.py
+++ controllable_LLMs/executable_batch.py
@@ -2,7 +2,7 @@ from controllable_LLMs.agents.call_to_action_agent import CallToActionAgent
 from controllable_LLMs.agents.context_agent import ContextAgent
 from controllable_LLMs.agents.framing_agent import FramingAgent
 from controllable_LLMs.agents.target_group_agent import TargetGroupAgent
-from .agents.example_agent import ExampleAgent
+from .agents.batch_agent import BatchAgent
 from .agents.emotion_agent import EmotionAgent
 from .agents.otherness_agent import OthernessAgent
 from .agents.intent_agent import IntentAgent
@@ -14,8 +14,8 @@ import pandas as pd
 
 model = "mistral-small"
 
-df = pd.read_csv("data/testdata/processed_VideoCommentsThreatCorpus.csv")
 grouped_df = pd.read_csv("data/testdata/grouped_processed_VideoCommentsThreatCorpus.csv")
+
 
 ### Due to the size of the topic threads, they haev been split into chunks ###
 
@@ -24,3 +24,20 @@ grouped_messages = grouped_df
 ###TESTING###
 grouped_messages = grouped_messages.sample(n=3)
 ###TESTING###
+
+batch_agent = BatchAgent(model)
+
+# Prepare DataFrame to store results
+collected_data = pd.DataFrame(columns=['document_id', 'extremism_labels'])
+
+for index, row in grouped_messages.iterrows():
+    content_list = row['content']
+    content = "\n\n".join(content_list)
+    results = []
+    for i in content_list:
+        results.append(batch_agent.__call__(i))
+
+    print(results)
+
+
+
