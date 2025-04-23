4bc47ff6c40c586a4f380932c6836a5e0f5244c9
Thomas Addisu Frette <thomas.frette@gmail.com>
2025-04-23 12:05:16 +0200

    change model for batch

--- src/batch.py
+++ src/batch.py
@@ -6,8 +6,8 @@ from datetime import datetime
 import pandas as pd
 
 # model = "mistral"
-# model = "mistral-nemo"
-model = "mistral-small"
+model = "mistral-nemo"
+# model = "mistral-small"
 
 grouped_df = pd.read_csv("data/testdata/grouped_processed_VideoCommentsThreatCorpus.csv")
 grouped_messages = grouped_df
