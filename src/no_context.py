7499e017842a818cc669ae4f83bc3b551c087645
Elias Borge Svinø <94860474+eliasborge@users.noreply.github.com>
2025-04-30 12:14:02 +0200

    Remove print statements

--- src/no_context.py
+++ src/no_context.py
@@ -7,9 +7,9 @@ from .agents.message_validation_agent import MessageValidationAgent
 from datetime import datetime
 import pandas as pd
 
-# model = "mistral"
+model = "mistral"
 # model = "mistral-nemo"
-model = "mistral-small"
+#model = "mistral-small"
 
 df = pd.read_csv("data/testdata/processed_VideoCommentsThreatCorpus.csv")
 grouped_df = pd.read_csv("data/testdata/grouped_processed_VideoCommentsThreatCorpus.csv")
@@ -21,7 +21,7 @@ grouped_messages = grouped_df
 
 mode="no-context"
 ###TESTING###
-grouped_messages = grouped_messages.head(5)
+grouped_messages = grouped_messages
 ###TESTING###
 ### Due to the size of the topic threads, they haev been split into chunks ###
 
@@ -46,26 +46,28 @@ for index,row in grouped_messages.iterrows():
     conversation_length = row['content_length']
     list_of_ids:list = row['id'].split(", ")
 
+    print(f"Processing row {index + 1} of {len(grouped_messages)}...")
+
     # print(list_of_ids)
 
     for index,post in df[df['id'].isin(list_of_ids)].iterrows():
         specific_post_content = post['content']
 
         specific_post_otherness = otherness_agent.__call__(specific_post_content, context,mode=mode)
-        print(specific_post_otherness)
+    
 
         specific_post_framing = framing_agent.__call__(specific_post_content, context,mode=mode)
-        print(specific_post_framing)
+    
 
         specific_post_intent = intent_agent.__call__(specific_post_content, specific_post_otherness['targetGroup'], specific_post_framing, context,mode=mode)
-        print(specific_post_intent)
+       
 
         specific_post_call_to_action = call_to_action_agent.__call__(specific_post_content, specific_post_otherness['targetGroup'], specific_post_framing, context,mode=mode)
-        print(specific_post_call_to_action)
+     
 
-        ##TODO DO SOMETHING WITH THIS
+    
         specific_post_validation = message_validation_agent.__call__(specific_post_content, otherness_boolean = specific_post_otherness['othernessBoolean'], target_group = specific_post_otherness['targetGroup'], framing_style = specific_post_framing['framingStyle'], framing_tool = specific_post_framing['framingTool'], intent_of_violence=specific_post_intent, call_to_action=specific_post_call_to_action, context=context,mode=mode)
-        print(specific_post_validation)
+        
 
         new_row = {'document_id': post['id'], 'num_posts_in_conversation': num_posts_in_conversation, 
         'conversation_length': conversation_length,  
