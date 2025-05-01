ab1b2ca1d8cc50284f0ce2ed5f621ccb00fa45cf
Thomas Addisu Frette <thomas.frette@gmail.com>
2025-05-01 11:53:29 +0200

    Fix soloAgent

--- src/solo.py
+++ src/solo.py
@@ -1,10 +1,10 @@
 
 from .agents.solo_agent import SoloAgent
 from datetime import datetime
+from pydantic import ValidationError
 import pandas as pd
-import tracemalloc
 import time
-import psutil
+
 
 
 # model = "mistral"
@@ -14,8 +14,6 @@ model = "mistral-nemo"
 ### Logging
 timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M")
 start_time = time.time()
-tracemalloc.start()
-process = psutil.Process()
 ####
 
 grouped_df = pd.read_csv("data/testdata/grouped_processed_VideoCommentsThreatCorpus.csv")
@@ -25,19 +23,13 @@ grouped_messages = grouped_df
 solo_agent = SoloAgent(model)
 
 # Prepare DataFrame to store results
-collected_data = pd.DataFrame(columns=['video_num','document_id', 'violence_label'])
-efficiency_data = pd.DataFrame(columns=[
-    'row', 'row_duration_sec',
-    'memory_used_MB', 'peak_memory_MB',
-    'cpu_user_time_sec', 'cpu_system_time_sec', 'cpu_total_time_sec'
-])
+collected_data = pd.DataFrame(columns=['video_num','document_id', 'violence_label', 'flagged_issue', 'row_duration_sec'])
 
 
 print("starting solo processing")
 
 for index, row in grouped_messages.iterrows():
     row_start_time = time.time()
-    cpu_start = process.cpu_times()
     content = row['content']
     list_of_ids:list = row['id'].split(", ")
     print(f"Processing row {index + 1} of {len(grouped_messages)}...")  
@@ -45,40 +37,23 @@ for index, row in grouped_messages.iterrows():
     results = []
 
     for i in content_list:
-        result = solo_agent.__call__(i)
+        try:
+            result = solo_agent.__call__(i)
+        except ValidationError as e:
+            print(f"Validation error for content: {i}, Error: {e}")
+            result = {'violent_label': None, 'flagged_issues': [1]}
         results.append(result)
 
+    row_duration_sec = time.time() - row_start_time
+
     for i, flag in enumerate(results):
-        new_row = {'video_num': list_of_ids[i].split('_')[0],'document_id':list_of_ids[i], 'violence_label': flag['violent_label'], 'flagged_issues': flag['flagged_issues']}
+        new_row = {'video_num': list_of_ids[i].split('_')[0],'document_id':list_of_ids[i], 'violence_label': flag['violent_label'], 'flagged_issues': flag['flagged_issues'], 'row_duration_sec': row_duration_sec}
         new_row_df = pd.DataFrame([new_row])
         collected_data = pd.concat([collected_data, new_row_df], ignore_index=True)
 
-    cpu_end = process.cpu_times()
-    cpu_user = cpu_end.user - cpu_start.user
-    cpu_system = cpu_end.system - cpu_start.system
-    cpu_total = cpu_user + cpu_system
-
-    row_duration = time.time() - row_start_time
-    current, peak = tracemalloc.get_traced_memory()
-    mem_used = current / 1e6
-    peak_mem = peak / 1e6
-
-    efficiency_data = pd.concat([
-        efficiency_data, 
-        pd.DataFrame([{
-            'row': index + 1,
-            'row_duration_sec': row_duration,
-            'memory_used_MB': mem_used,
-            'peak_memory_MB': peak_mem,
-            'cpu_user_time_sec': cpu_user,
-            'cpu_system_time_sec': cpu_system,
-            'cpu_total_time_sec': cpu_total
-        }])
-    ], ignore_index=True)
 
     
 timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M")
 collected_data.to_csv(f"data/testdata/test_results_from_idun/solo/solo{model}_{timestamp}.csv", index=False)
-efficiency_data.to_csv(f"data/testdata/test_results_from_idun/solo/solo_efficiency{model}_{timestamp}.csv", index=False)
 print("solo processing completed and results saved.")
 
