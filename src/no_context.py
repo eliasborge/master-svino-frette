ebd7bc62d15863304c43756aff10fb91f09231e5
Thomas Addisu Frette <thomas.frette@gmail.com>
2025-05-02 21:36:03 +0200

    change model

--- src/no_context.py
+++ src/no_context.py
@@ -8,10 +8,11 @@ from .agents.classification_agent import ClassificationAgent
 from datetime import datetime
 import pandas as pd
 
-#model = "mistral"
+model = "mistral"
 #model = "mistral-nemo"
 #model = "mistral-small"
-model = "gemma3:27b"
+# model = "gemma3:27b"
+
 df = pd.read_csv("data/testdata/processed_VideoCommentsThreatCorpus.csv")
 grouped_df = pd.read_csv("data/testdata/grouped_processed_VideoCommentsThreatCorpus.csv")
 
