5c3c0d3c83e9704578ffe6ea6e6e8912e34e8d07
Thomas Addisu Frette <thomas.frette@gmail.com>
2025-05-07 21:07:30 +0200

    fix

--- src/context_analysis.py
+++ src/context_analysis.py
@@ -10,9 +10,9 @@ from datetime import datetime
 
 import pandas as pd
 
-#model = "mistral"
+model = "mistral"
 # model = "mistral-nemo"
-model = "mistral-small"
+# model = "mistral-small"
 # model = "gemma3:27b"
 
 df = pd.read_csv("data/testdata/processed_VideoCommentsThreatCorpus.csv")
