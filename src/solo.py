f4addc9b95ebaa160fd800d0286af901b073a950
Thomas Addisu Frette <thomas.frette@gmail.com>
2025-05-07 21:11:47 +0200

    add new model

--- src/solo.py
+++ src/solo.py
@@ -11,7 +11,8 @@ import time
 # model = "mistral-nemo"
 # model = "mistral-small"
 # model = "gemma3:27b"
-model = "gemma3:12b"
+#model = "gemma3:12b"
+model = "qwen3:8b"
 
 ### Logging
 timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M")
